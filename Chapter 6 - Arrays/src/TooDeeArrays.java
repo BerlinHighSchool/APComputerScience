826af721a06c49b6198e0b10dd585f250b9f11d8
mcwirka <michael.cwirka@gmail.com>
2017-10-25 12:40:36 -0400

    2d arrays

---
+++ Chapter 6 - Arrays/src/TooDeeArrays.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class TooDeeArrays {
+	public static void main(String[] args) {
+		int[][] numarray = new int[5][5];
+		Random g = new Random();
+		
+		for(int r = 0; r < numarray.length; r++)
+			for(int c = 0; c < numarray[r].length; c++)
+				numarray[r][c] = g.nextInt(25)+3;
+		
+		for(int r = 0; r < numarray.length; r++) {
+			for(int c = 0; c < numarray[r].length; c++)
+				System.out.print(numarray[r][c] + "\t");
+			System.out.println();
+		}
+		
+		// Generate a 3x3 2D array randomly filled with x's and o's
+		// Determine how many 3 x's or 3 o's are in a row (vertical
+		// horizontal and/or diagonal.
+	}
+}
