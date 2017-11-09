0506f5499a8d75940652390aa9dcdd06e0fe5e07
mcwirka <michael.cwirka@gmail.com>
2017-11-09 10:30:49 -0500

    gui finish

--- GUI/src/DeckofCards.java
+++ GUI/src/DeckofCards.java
@@ -1,11 +1,20 @@
 import java.awt.Dimension;
+import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
 public class DeckofCards {
-	int[] deck = new int[13];
+	Integer[] deck = new Integer[13];
 	static JLabel s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, sj, sq, sk;
 	static JFrame frame = new JFrame("Deck of Cards");
+	static int place = 0, cardx = 20, cardy = 126;
+	static JButton exit = new JButton("Exit");
+	
+	// Deck face
+	static java.net.URL deckFace = DeckofCards.class.getResource("resources/b2fv.gif");
+	static ImageIcon df = new ImageIcon(deckFace);
+	
+	static JButton deal = new JButton(df);
 	
 	//Ace of Spades
 	static java.net.URL spades1 = DeckofCards.class.getResource("resources/s1.gif");
@@ -59,10 +68,12 @@ public class DeckofCards {
 	static java.net.URL spadesK = DeckofCards.class.getResource("resources/sk.gif");
 	static ImageIcon sofK = new ImageIcon(spadesK);
 
+	ButtonListener listener = new ButtonListener();
+	
 	public DeckofCards() {
 		// Fill the array with values 1-13 representing our cards
 		// 1 is Ace --> 13 for King
-		for(int i = 0; i <deck.length; i++)
+		for(int i = 0; i < deck.length; i++)
 			deck[i] = i+1;
 
 		// Shuffle the deck
@@ -89,12 +100,95 @@ public class DeckofCards {
 		frame.setLayout(null);
 		frame.setPreferredSize(new Dimension(800, 600));
 		
-		sk.setBounds(20, 20, 71, 96);
-		frame.add(sk);
+		deal.setBounds(20, 20, 71, 96);
+		deal.addActionListener(listener);
+		//deal.setBorder(null);
+		//deal.setMargin(new Insets(0, 0, 0, 0));
+		
+		exit.setBounds(105, 20, 100, 30);
+		exit.addActionListener(listener);
+		
+		frame.add(deal);
+		frame.add(exit);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	public class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == exit) {
+				System.exit(0);
+			}
+			if(e.getSource() == deal) {
+				int card = deck[place];
+				place++;
+				
+				switch(card) {
+				case 1:
+					frame.add(s1);
+					s1.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 2:
+					frame.add(s2);
+					s2.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 3:
+					frame.add(s3);
+					s3.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 4:
+					frame.add(s4);
+					s4.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 5:
+					frame.add(s5);
+					s5.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 6:
+					frame.add(s6);
+					s6.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 7:
+					frame.add(s7);
+					s7.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 8:
+					frame.add(s8);
+					s8.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 9:
+					frame.add(s9);
+					s9.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 10:
+					frame.add(s10);
+					s10.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 11:
+					frame.add(sj);
+					sj.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 12:
+					frame.add(sq);
+					sq.setBounds(cardx, cardy, 71, 96);
+					break;
+				case 13:
+					frame.add(sk);
+					sk.setBounds(cardx, cardy, 71, 96);
+					break;
+				}
+				
+				cardx+= 80;
+				if(place % 5 == 0) {
+					cardy += 120;
+					cardx = 20;
+				}
+				if(place > deck.length-1)
+					deal.setEnabled(false);
+			}
+		}
 	}
 }
 
